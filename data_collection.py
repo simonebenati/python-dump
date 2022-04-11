b8e208d23a56f5d1e4bfc6d9838ff89b052b122f
Simone Benati <simone.benati@outlook.com>
2022-04-11 22:56:50 +0200

    added comments to describe tasks

--- data_collection.py
+++ data_collection.py
@@ -3,7 +3,7 @@
 import pyperclip
 import re
 
-
+#this regex identifies and us/canadian phone number
 
 regex_phone = re.compile(r"""
 (
@@ -18,6 +18,8 @@ regex_phone = re.compile(r"""
 """
 , re.VERBOSE)
 
+#this regex identifies an email
+
 regex_email = re.compile(r"""
 [a-zA-Z0-9_.+]+                                 #part1 simone.benati@outlook.com.com I made this ((\w+) | (\w+\.\w+)) but guide uses
 \@                                                # at sign @
@@ -29,16 +31,18 @@ regex_email = re.compile(r"""
 
 """,re.VERBOSE)
 
-document = pyperclip.paste()
+#we paste the document we want to analyze into a var document
 
+document = pyperclip.paste()
+#we search for phone numbers and emails
 mo_phone = regex_phone.findall(document)
 mo_mail = regex_email.findall(document)
-
+#we create a cleaner output for phone findall 
 new_mo_phone = []
 
 for p_numbers in mo_phone: #for clear phone number viewing: findall() returns a list of tuples we need to iterate each tuple and return the first element of it so: tuple[0]
     new_mo_phone.append(p_numbers[0])
-
+#we create a list by joining two lists
 parsed_document = '\n'.join(new_mo_phone) + '\n' + '\n'.join(mo_mail)
 
 
